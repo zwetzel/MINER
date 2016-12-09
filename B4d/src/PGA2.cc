6ac82542215c0b9180d046db8bbd91a7b047e4d5
zwetzel <zwetzel@tamu.edu>
2016-12-09 15:00:21 -0600

    Create PGA2.cc

---
+++ B4d/src/PGA2.cc
@@ -0,0 +1,43 @@
+#include "PGA2.hh"
+#include "G4RunManager.hh"
+#include "G4LogicalVolumeStore.hh"
+#include "G4LogicalVolume.hh"
+#include "G4Box.hh"
+#include "G4Event.hh"
+#include "G4GeneralParticleSource.hh"
+#include "G4ParticleGun.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
+#include "G4SystemOfUnits.hh"
+#include "Randomize.hh"
+#include "globals.hh"
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PGA2::PGA2()
+{ m_particleGun = new G4GeneralParticleSource();
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+PGA2::~PGA2()
+
+{
+  delete m_particleGun;
+}
+
+//....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
+
+void PGA2::GeneratePrimaries(G4Event* anEvent)
+{
+  // This function is called at the begining of event
+
+  // In order to avoid dependence of PrimaryGeneratorAction
+  // on DetectorConstruction class we get world volume
+  // from G4LogicalVolumeStore
+  //
+  // Set gun position
+
+  m_particleGun->GeneratePrimaryVertex(anEvent);
+}
+
